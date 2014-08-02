b7c39f2a376bdbf878fa216705bf8f5f983d8196
cclauss <cclauss@bluewin.ch>
2014-08-02 10:40:38 +0200

    Detect if there are enough command line arguments

--- lpfinder.py
+++ lpfinder.py
@@ -1,36 +1,39 @@
-# this takes 2 arguments
-# 1) the name of the account
-# 2) the url to be redirected to
-# best if used with Launch Center Pro
-# https://launchcenterpro.com/q9qmfg 
+welcome_msg = '''This script takes 2 arguments:
+1) the name of the account
+2) the url to be redirected to
+It works best with Launch Center Pro installed.
+https://launchcenterpro.com/q9qmfg'''
 
-from sys import argv
-import keychain
-import webbrowser
-import ui
-import clipboard
-import console
-services = keychain.get_services()
+import clipboard, console, keychain, sys, ui, webbrowser
+
+try:
+    _, account_name, redirect_url = sys.argv
+    account_name = account_name.lower()
+except ValueError:
+    print(welcome_msg)
+    sys.exit()
 
 @ui.in_background
 def item_selected(sender):
-	acct = sender.items[sender.selected_row]['title'].split(' - ')
-	pwd = keychain.get_password(acct[0],acct[1])
-	clipboard.set(pwd)
-	picker.close()
-	webbrowser.open(argv[2])
+    acct = sender.items[sender.selected_row]['title'].split(' - ')
+    pwd = keychain.get_password(acct[0],acct[1])
+    clipboard.set(pwd)
+    picker.close()
+    webbrowser.open(redirect_url)
 
 @ui.in_background
 def info_tapped(sender):
-	row = sender.items[sender.tapped_accessory_row]
-	console.alert(row['title'])
-ds = ui.ListDataSource({'title':(x[0] +' - '+ x[1]),'accessory_type':'detail_button'} for x in services if argv[1].lower() in x[0].lower())
+    row = sender.items[sender.tapped_accessory_row]
+    console.alert(row['title'])
+
+services = [ (x[0] + ' - ' + x[1]) for x in keychain.get_services()
+                                    if account_name in x[0].lower() ]
+ds = ui.ListDataSource({'title':x,'accessory_type':'detail_button'} for x in services)
 ds.action= item_selected
 ds.accessory_action = info_tapped
 
 picker = ui.load_view('lpfinder')
 table = picker.subviews[0]
-table.data_source = ds
-table.delegate = ds
+table.data_source = table.delegate = ds
 
 picker.present('sheet')
