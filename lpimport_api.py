0ff1191d54ca6f92576d89d1ccda9a7334c1b668
cclauss <cclauss@bluewin.ch>
2014-08-02 10:07:14 +0200

    Use or '' to eliminate the need for xstr()

--- lpimport_api.py
+++ lpimport_api.py
@@ -1,41 +1,25 @@
-# this saves the LastPass vault to the iOS keychain
-# from my understanding - although the keychain is encrypted
-# it is not 100% secure
-# so use with caution
-# at a minimum you probably should `keychain.set_master_password()`
+# Use with caution -- This saves the LastPass vault in the iOS keychain.
+# Although the keychain is encrypted, I believe it is not 100% secure.
+# At a minimum you probably should `keychain.set_master_password()`
+ 
+import clipboard, console, keychain, lastpass, uuid, webbrowser
 
-import lastpass
-import keychain
-import console
-import webbrowser
-import uuid
-import clipboard
-
-def xstr(s):
-    return '' if s is None else str(s)
-
-email = xstr(keychain.get_password('lastpass_email', 'lastpass'))
-
-password = xstr(keychain.get_password('lastpass_master', 'lastpass'))
-
-email,password = console.login_alert('LastPass login', '', email, password)
-
+email = keychain.get_password('lastpass_email', 'lastpass') or ''
+password = keychain.get_password('lastpass_master', 'lastpass') or ''
+email, password = console.login_alert('LastPass login', '', email, password)
 device = keychain.get_password('lastpass_uuid', 'lastpass')
 
 try:
     vault = lastpass.Vault.open_remote(email, password, None, device)
-    
 except lastpass.LastPassIncorrectGoogleAuthenticatorCodeError as e:
-
     googleauth = None
-    if device is None:
+    if not device:
         webbrowser.open('otpauth:')
         googleauth = console.input_alert('GoogleAuth', '', clipboard.get())
         trusted = console.alert('Trusted', 'Save this device as trusted?', 'Save', 'Don\'t Save', hide_cancel_button=True)
-        if trusted == 0:
+        if not trusted:
             device = str(uuid.uuid1())
             keychain.set_password('lastpass_uuid', 'lastpass', device)
-
     vault = lastpass.Vault.open_remote(email, password, googleauth, device)
 
 for i in vault.accounts:
