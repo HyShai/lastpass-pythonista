789fc9517095790dee5297f5ca4a6b2d8b50975c
shaya <hyshai29@gmail.com>
2014-08-04 10:00:22 -0400

    continue refactor

--- lpfinder.py
+++ lpfinder.py
@@ -10,16 +10,15 @@ import clipboard, console, keychain, sys, ui, webbrowser, os
 try:
     _, account_name, redirect_url = sys.argv
     account_name = account_name.lower()
-    # make a global to be able to access the decrypted blob in `item_selected`
-    accounts_from_blob = None
+
 except ValueError:
     print(welcome_msg)
     sys.exit()
 
 
 class AccountFinder(object):
-    def __init__(self, blob):
-        self.accounts = get_services(blob)
+    def __init__(self, blob=None):
+        self.accounts = self.get_services(blob)
         self.has_blob = (True if blob else False)
 
     def get_services(self, blob=None):
@@ -28,8 +27,7 @@ class AccountFinder(object):
             email = keychain.get_password('lastpass_email', 'lastpass') or ''
             password = keychain.get_password('lastpass_master', 'lastpass') or ''
             email, password = console.login_alert('LastPass login', '', email, password)
-            return [x.name, x.username, x.password
-                        for x in lastpass.Vault.open_local(blob)]
+            return [(x.name, x.username, x.password) for x in lastpass.Vault.open_local(blob)]
         else:
             return keychain.get_services()
 
@@ -41,52 +39,37 @@ class AccountFinder(object):
 
     def find_matching_accounts(self, account_name):
         return [(x[0] + ' - ' + x[1]) for x in self.get_services()
-                if account_name in x[0].lower()]
+            if account_name in x[0].lower()]
 
 
+class AccountFinderView (ui.View):
+    def __init__(self):
+        FILENAME = os.path.join(os.getcwd(), '.lastpass.blob')
+        self.AccountFinder = AccountFinder(blob=FILENAME if os.path.exists(FILENAME)else None)
+        ds = ui.ListDataSource({'title':x, 'accessory_type':'detail_button'} for x in self.AccountFinder.find_matching_accounts(account_name))
+        ds.action = self.item_selected
+        ds.accessory_action = self.info_tapped
+        tv = ui.TableView()
+        tv.frame = self.bounds
+        tv.flex = 'WH'
+        tv.data_source = tv.delegate = ds
+
+        self.add_subview(tv)
+
+    @ui.in_background
+    def item_selected(self, sender):
+        acct = sender.items[sender.selected_row]['title'].split(' - ')
+        self.close()
+        # there's a bug here - keychain.master_password hangs the ui even though @ui.in_background
+        pwd = self.AccountFinder.get_password(acct[0], acct[1])    
+        clipboard.set(pwd)
+        webbrowser.open(redirect_url)
+
+    @ui.in_background
+    def info_tapped(self, sender):
+        row = sender.items[sender.tapped_accessory_row]
+        console.alert(row['title'])
 
 
-@ui.in_background
-def item_selected(sender):
-    acct = sender.items[sender.selected_row]['title'].split(' - ')
-    # if using local blob
-    if accounts_from_blob:
-        # what's the pythonic way to do this?
-        pwd = [x.password for x in accounts_from_blob if x.name.lower() == acct[0] and x.username == acct[1]][0]
-    else:
-        pwd = keychain.get_password(acct[0], acct[1])
-
-    clipboard.set(pwd)
-    picker.close()
-    webbrowser.open(redirect_url)
-
-
-@ui.in_background
-def info_tapped(sender):
-    row = sender.items[sender.tapped_accessory_row]
-    console.alert(row['title'])
-
-
-def get_services():
-    if os.path.isfile('.lastpass.blob'):
-        import lastpass
-        email = keychain.get_password('lastpass_email', 'lastpass') or ''
-        password = keychain.get_password('lastpass_master', 'lastpass') or ''
-        email, password = console.login_alert('LastPass login', '', email, password)
-        accounts_from_blob = lastpass.Vault.open_local('.lastpass.blob')
-        # what's the pythonic way to do this?
-        return [{x.name, x.username} for x in accounts_from_blob]
-    else:
-        return keychain.get_services()
-
-matching_services = [(x[0] + ' - ' + x[1]) for x in get_services()
-                                    if account_name in x[0].lower()]
-ds = ui.ListDataSource({'title':x, 'accessory_type':'detail_button'} for x in matching_services)
-ds.action = item_selected
-ds.accessory_action = info_tapped
-
-picker = ui.load_view('lpfinder')
-table = picker.subviews[0]
-table.data_source = table.delegate = ds
-
-picker.present('sheet')
+account_finder_view = AccountFinderView()
+account_finder_view.present('sheet')
